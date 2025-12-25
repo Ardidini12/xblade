5dae0ad49c924f9adeb3f834fa047913d2bf8eb4
Ardidini12 <161293115+Ardidini12@users.noreply.github.com>
2025-12-25 20:20:01 +0100

    implemented basic auth layouts, signup and signin

---
+++ types/global.d.ts
@@ -0,0 +1,34 @@
+declare global {
+    interface SignUpFormData {
+        fullName: string;
+        email: string;
+        gamertag: string;
+        password: string;
+    }
+
+    type SignInFormData = {
+        email: string;
+        password: string;
+    };
+
+    type FormInputProps = {
+        name: string;
+        label: string;
+        placeholder: string;
+        type?: string;
+        register: UseFormRegister;
+        error?: FieldError;
+        validation?: RegisterOptions;
+        disabled?: boolean;
+        value?: string;
+    }
+
+    type FooterLinkProps = {
+        text: string;
+        linkText: string;
+        href: string;
+    }
+        ;
+}
+
+export { };
